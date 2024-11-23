915d7b731fe90ea5aee640ab9ec448ad476cdedb
Yaroslav <danissimokuvshin13@gmail.com>
2024-11-23 18:50:41 +0300

    Add more symbols; Change QK_MACRO 6 and 7 str

--- keyboards/ergohaven/ergohaven_keycode_str.c
+++ keyboards/ergohaven/ergohaven_keycode_str.c
@@ -365,7 +365,11 @@ const char *special_keycode_to_str(uint16_t keycode) {
             return EH_SYMBOL_LAYER EH_SYMBOL_ANGLES_LEFT;
         case QK_MACRO ... QK_MACRO_MAX:
             if (keycode == QK_MACRO_8)
-                return "M8 " EH_SYMBOL_BIRD;
+                return "M8\n" EH_SYMBOL_BIRD;
+            else if (keycode == QK_MACRO_7)
+                return "M7\n" EH_SYMBOL_WHEELCHAIR;
+            else if (keycode == QK_MACRO_6)
+                return "M6 " EH_SYMBOL_GAMEPAD;
             else
                 sprintf(buf, "M%d", keycode - QK_MACRO);
             return buf;
